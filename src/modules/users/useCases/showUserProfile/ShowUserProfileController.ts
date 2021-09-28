c260d9e118f3cfdcee8f59fa93d41dddaa4388d2
gabrieldk <ganriel201@gmail.com>
2021-09-28 14:41:43 -0300

    show proffile

--- src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
@@ -6,7 +6,16 @@ class ShowUserProfileController {
   constructor(private showUserProfileUseCase: ShowUserProfileUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.params;
+
+    let user = null;
+    try {
+      user = this.showUserProfileUseCase.execute({ user_id });
+    } catch (error) {
+      return response.status(404).json({ error });
+    }
+
+    return response.json(user);
   }
 }
 
